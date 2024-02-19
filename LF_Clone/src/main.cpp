bb475c7c6c02d8f3a9c1076444cc5c64909b75ac
Vojtěch Řehák <vojtech.rehak@warhorsestudios.cz>
2024-02-20 00:36:23 +0100

    added imgui sfml framework for GUI

--- LF_Clone/src/main.cpp
+++ LF_Clone/src/main.cpp
@@ -1,6 +1,14 @@
+#include "imgui.h"
+#include "imgui-SFML.h"
 
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+
+#include <iostream>
 
 int main()
 {
-   
+    
 }
