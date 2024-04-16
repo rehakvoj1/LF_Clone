92454757843e3913c0fce0cf27a466fde90c16a9
Vojtěch Řehák <vojtech.rehak@warhorsestudios.cz>
2024-04-16 23:43:13 +0200

    deleted obsolete code

--- LF_Clone/src/main.cpp
+++ LF_Clone/src/main.cpp
@@ -30,29 +30,4 @@ int main(int argc, char** argv)
         return -1;
     }
     engine.Run();
-
-    // init game objects
-
-    // player
-    //sf::Sprite spriteSorcerer( sorcererTex, sf::IntRect( { 0,0 }, { 80,80 } ) );
-    //sf::Sprite spriteSorcererMirror( sorcererTexMirror, sf::IntRect( { 720,0 }, { 80,80 } ) );
-    
-    
-    
-    //// enemy
-    //sf::Sprite spriteEnemy( enemyTex, sf::IntRect( { 0,0 }, { 80,80 } ) );
- 
-
-
-        // render
-        //window->clear( sf::Color::Cyan );
-        //player.OnRender( *window );  // projectile is rendered inside ---> Not good
-        //enemy.OnRender( *window );   // projectile is rendered inside ---> Not good
-        
-        //ImGui::Begin( "healthbar",0,ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoInputs );
-        //ImGui::DrawRectFilled( sf::FloatRect( { 80,0 }, { 100,20 } ), sf::Color::Black );
-        ////ImGui::DrawRectFilled( sf::FloatRect( { 80,0 }, { player.GetHealth(),20}), sf::Color::Red);
-        //ImGui::End();
-        //ImGui::SFML::Render( *window );  // -----> renders ImGui elements
-        //window->display();   // -----------------> display rendered elements to window 
 }
